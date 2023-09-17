a0530358f12d8ae131878722f30d8e6a20347e49
Tyson Cheung <113953467+ch-tyson@users.noreply.github.com>
2023-09-17 21:01:33 +0000

    Next.js initialize

---
+++ art-galleria/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Art Galleria',
+  description: 'a collection of images, app by tyson cheung',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="max-w-6xl mx-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
