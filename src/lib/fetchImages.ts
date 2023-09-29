4af6b924b67f8d7929c7a436f423c81fcefe1eec
Tyson Cheung <113953467+ch-tyson@users.noreply.github.com>
2023-09-29 01:40:33 +0000

    typesafety w/ envalid to env vars

--- src/lib/fetchImages.ts
+++ src/lib/fetchImages.ts
@@ -1,19 +1,19 @@
-import type { ImageResults } from "@/models/Images"; 
-import { ImagesSchemaWithPhotos } from "@/models/Images";
-import { resolveSoa } from "dns";
+import type { ImagesResults } from "@/models/Images"; 
+import { ImagesSchemaWithPhotos } from "@/models/Images"
+import env from "./env"
 
 export default async function fetchImages(url: string): 
-Promise<ImageResults | undefined> { 
+Promise<ImagesResults | undefined> { 
     try {
         const res = await fetch(url, {
             headers : {
-                Authorization: process.env.PEXELS_API_KEY
+                Authorization: env.PEXELS_API_KEY
             }
         })
 
         if (!res.ok) throw new Error ("Fetch Images error!\n")
 
-        const imagesResults: ImageResults = await res.json();
+        const imagesResults: ImagesResults = await res.json()
 
         console.log(imagesResults)
         // Parse data with Zod schema
