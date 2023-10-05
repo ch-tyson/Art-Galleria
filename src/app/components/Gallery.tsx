3bccb44f5a09985e0eac44fabcfa28ece02e144c
Tyson Cheung <113953467+ch-tyson@users.noreply.github.com>
2023-10-05 21:56:35 +0000

    Create basic image component

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -12,7 +12,7 @@ export default async function Gallery() {
     <section className = "px-2 my-3 grid gap-2 grid-cols-gallery">
         
             {images.photos.map(photo => (
-                <div key={photo.id} className="h-64 bg-gray-200 rounded-xl"></div>
+                
             ))}
         
     </section>
